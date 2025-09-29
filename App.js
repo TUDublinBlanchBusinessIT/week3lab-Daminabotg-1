f895a33d45bb2e368e41482b61400f24aa515fdc
B00161001 Daminabo Tom-George <B00161001@mytudublin.ie>
2025-09-29 16:53:59 +0100

    Completed Part 1

--- App.js
+++ App.js
@@ -1,16 +1,20 @@
 import {View, Text, TextInput} from 'react-native';
 import React, {useState} from 'react';
-
+import Logo from './components/Logo';
 
 export default function App(){
   const [fullname, setFullname] = useState("Eoin");
+  const [fname, setFname] = useState("Joe");
+  const [lname, setLname] = useState("Bloggs");
+  const [dob, setDob] = useState("22 August 1990");
   return(
-    <View> 
+    <View>
+      <Logo/> 
       <Text>Hello, World {fullname}</Text>
       <TextInput placeholder="enter your name" onChangeText={(value)=>setFullname(value)}></TextInput>
     </View>
   )
-};
+}
 
 
 
